e7e2865284260de013c2ea6d9275fa237a2e3885
Chris <zc850926@163.com>
2016-05-09 11:40:23 +0800

    init service autorestart
    init service autorestart

---
+++ Config.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAutoRestart
+{
+    class Config
+    {
+        public string GetHour()
+        {
+            string hour="";
+
+            FileStream aFile = new FileStream("C:/reboot/config.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(aFile);
+            string strLine = "";
+            strLine = sr.ReadLine();
+            //string num = "";
+            //Read data in line by line 这个兄台看的懂吧~一行一行的读取     
+            while(strLine != null)
+            {
+                if (strLine != null)
+                {
+                    hour = strLine;
+                }
+                //Console.WriteLine(strLine);
+                strLine = sr.ReadLine();
+                
+            }
+            sr.Close();
+            return hour;
+        }
+        public void Reboot()
+        {
+            ProcessStartInfo ps = new ProcessStartInfo(); 
+            ps.FileName = "shutdown.exe"; 
+            ps.Arguments = "-r -t 1"; 
+            Process.Start(ps); 
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ServiceAutoRestart
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+           System.Timers.Timer tReboot = new System.Timers.Timer(30000);
+           tReboot.Elapsed += new ElapsedEventHandler(Reboot);
+           tReboot.AutoReset = true;
+           tReboot.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+        }
+        private void Reboot(object sender, ElapsedEventArgs e)
+        {
+            Config myconfig = new Config();
+            if (DateTime.Now.Hour.ToString().Equals(myconfig.GetHour()) && DateTime.Now.Minute.ToString().Equals("0"))
+            {
+                myconfig.Reboot();
+            }
+ 
+        }
+    }
+}
